86ce38ac1cfbe2f7dd5dd5f88371de787fd4a28c
Wesley <wesleybaesso6@gmail.com>
2026-09-22 07:52:55 -0300

    Realizações feitas no dia 21/09/2026

---
+++ function_app.py
@@ -0,0 +1,26 @@
+import logging
+import azure.functions as func
+
+app = func.FunctionApp()
+
+@app.schedule(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=True,
+              use_monitor=False) 
+def timer_trigger_turmac(myTimer: func.TimerRequest) -> None:
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Hello Word!')
+
+@app.route(route="http_trigger", auth_level=func.AuthLevel.ANONYMOUS)
+def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
